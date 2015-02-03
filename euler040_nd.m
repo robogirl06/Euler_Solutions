941504743b277bedd9b3ab9bb96a5c75e1798680
robogirl06 <asadvaney@gmail.com>
2015-02-03 17:18:30 -0500

    Rename euler040.m to euler040_nd.m

--- euler040_nd.m
+++ euler040_nd.m
@@ -9,7 +9,7 @@
 % If dn represents the nth digit of the fractional part, find the value of
 % the following expression.
 % 
-% d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+% d1 Ã— d10 Ã— d100 Ã— d1000 Ã— d10000 Ã— d100000 Ã— d1000000
 
 clear all
 clc
